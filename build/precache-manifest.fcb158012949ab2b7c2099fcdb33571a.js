023da453513c2f86ccb917822d56ed9ce80f9423
Vibhor Sehgal <nurvs@nus.edu.sg>
2018-12-09 10:36:35 +0530

    Build Folder

---
+++ build/precache-manifest.fcb158012949ab2b7c2099fcdb33571a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "df9bc5dfcaa7e57bf4a9",
+    "url": "/academicportfolio/static/js/runtime~main.df9bc5df.js"
+  },
+  {
+    "revision": "0ecbafc689efb87e0302",
+    "url": "/academicportfolio/static/js/main.0ecbafc6.chunk.js"
+  },
+  {
+    "revision": "e8f29d7cdba6718ac2de",
+    "url": "/academicportfolio/static/js/1.e8f29d7c.chunk.js"
+  },
+  {
+    "revision": "0ecbafc689efb87e0302",
+    "url": "/academicportfolio/static/css/main.c9ba6345.chunk.css"
+  },
+  {
+    "revision": "e8f29d7cdba6718ac2de",
+    "url": "/academicportfolio/static/css/1.b938ea4e.chunk.css"
+  },
+  {
+    "revision": "29ca29ee83cd1560ad2e6f13cc9b62ca",
+    "url": "/academicportfolio/index.html"
+  }
+];
